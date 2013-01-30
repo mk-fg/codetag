79a7fb55f96cf8127f5d7b3e4117c7d5188ee2b9
Mike Kazantsev <mk.fraggod@gmail.com>
2013-01-30 20:57:03 +0600

    Some decent logging and formatting

--- default.do
+++ default.do
@@ -9,7 +9,7 @@ redo-ifchange $DEPS
 
 err=0
 ls -1 $DEPS | xargs -n1 golang_filter curl
-go build -o "$3" "${PROJECT}" || err=$?
+go build -o "$3" "${PROJECT}" >&2 || err=$?
 ls -1 $DEPS | xargs -n1 golang_filter uncurl
 
 exit $err
--- src/codetag/main.go
+++ src/codetag/main.go
@@ -2,8 +2,53 @@ package main
 
 import (
 	"fmt"
+	"flag"
+	"os"
+	"github.com/hoisie/mustache"
+	"github.com/vaughan0/go-logging"
 )
+	// "path/filepath"
+
+type path_t string
+func (path path_t) Render() string {
+	return fmt.Sprint(path)
+}
+
+var config_paths = []path_t{"~/.codetag.yaml", "/etc/codetag.yaml"}
+var config string
+
 
 func main() {
-	fmt.Println("hello world")
+
+	flag.Usage = func() {
+		fmt.Println(mustache.Render( `usage: {{{cmd}}} [ <options> ]
+
+Index code files, using parameters specified in the config file.
+If not specified exmplicitly, config file is searched within the
+following paths (in that order):
+{{#paths}}
+  - {{{Render}}}
+{{/paths}}
+Examples:
+  % {{{cmd}}}
+  % {{{cmd}}} --config config.yaml
+
+Options:`, map[string]string{"cmd": os.Args[0]}, map[string][]path_t{"paths": config_paths} ))
+		flag.PrintDefaults()
+	}
+
+	flag.StringVar(&config, "config", "", "Configuration file to use.")
+	flag.Parse()
+	if flag.NArg() > 0 {
+		fmt.Printf("Error: no command-line arguments are allowed (provided: %v)\n", flag.Args())
+		os.Exit(1)
+	}
+
+	logging.DefaultSetup()
+	log := logging.Get("codegen.main")
+
+	log.Infof("config: %#v", config)
 }
+
+	// func walk_iter(path string, info os.FileInfo, err error) error
+	// filepath.Walk(root string, walkFn WalkFunc) error
