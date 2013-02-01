450f5a5213c1f674bf37d00809464f208a7ac4de
Mike Kazantsev <mk.fraggod@gmail.com>
2013-02-01 12:23:15 +0600

    Initial logging (with setup from yaml) implementatiion

---
+++ src/codetag/log_setup/log_setup.go
@@ -0,0 +1,138 @@
+package log_setup
+
+import (
+	"fmt"
+	"strings"
+	"github.com/vaughan0/go-logging"
+	"github.com/kylelemons/go-gypsy/yaml"
+)
+
+
+// Unfortunately, logging doesn't export these.
+var reverseLevelStrings = map[string]logging.Level {
+	"FATAL": logging.Fatal,
+	"ERROR": logging.Error,
+	"WARN": logging.Warn,
+	"NOTICE": logging.Notice,
+	"INFO": logging.Info,
+	"DEBUG": logging.Debug,
+	"TRACE": logging.Trace,
+}
+
+
+// Loads the appropriate plugin and creates an outputter, given a configuration section.
+func newOutputterConfig(cp *yaml.Map) (logging.Outputter, error) {
+	config := *cp
+
+	// Convert yaml.Map to map[string]string{}
+	config_map := map[string]string{}
+	for k, node := range config {
+		v, ok := node.(yaml.Scalar)
+		if !ok {
+			return nil, fmt.Errorf("Invalid type: %v", node)
+		}
+		config_map[k] = string(v)
+	}
+
+	// Get/create plugin/outputter from the "type" option
+	name, ok := config_map["type"]
+	if !ok {
+		return nil, fmt.Errorf("Plugin name not specified: %v", config_map)
+	}
+	plugin, err := logging.GetOutputPlugin(name)
+	if plugin == nil {
+		return nil, err
+	}
+	output, err := plugin.CreateOutputter(config_map)
+	if err != nil {
+		return nil, err
+	}
+
+	// Check for the "threshold" option
+	thresh, ok := config_map["threshold"]
+	if ok {
+		level, ok := reverseLevelStrings[strings.ToUpper(thresh)]
+		if ok {
+			output = logging.ThresholdOutputter{level, output}
+		} else {
+			return nil, fmt.Errorf("Invalid threshold: %v", thresh)
+		}
+	}
+
+	return output, nil
+}
+
+
+// Configures the logging hierarchy from a YAML object
+func SetupYAML(cp *yaml.Map) (err error) {
+	config := *cp
+	// fmt.Println(config)
+
+	var (
+		section yaml.Node
+		section_map yaml.Map
+	)
+
+	// Create outputters
+	outputters := make(map[string]logging.Outputter)
+	for key, section := range config {
+		if key != "loggers" {
+			section_map, ok := section.(yaml.Map)
+			if ok {
+				output, err := newOutputterConfig(&section_map)
+				if err == nil {
+					outputters[key] = output
+					continue
+				}
+			}
+			return fmt.Errorf("Failed to init Outputter: %s", key)
+		}
+	}
+
+	section, ok := config["loggers"]
+	if !ok {
+		return fmt.Errorf("'loggers' section is must be a map")
+	}
+	section_map, ok = section.(yaml.Map)
+	if !ok {
+		return fmt.Errorf("'loggers' section is missing")
+	}
+
+	// Setup loggers
+	for name, node := range section_map {
+		v, ok := node.(yaml.Scalar)
+		if !ok {
+			return fmt.Errorf("Logging level must be string: %v", v)
+		}
+		parts := strings.Split(string(v), ",")
+		level, ok := reverseLevelStrings[strings.ToUpper(parts[0])]
+		if !ok {
+			return fmt.Errorf("Unknown logging level: %v", v)
+		}
+		// Get the logger by its name, treating "root" as a special name
+		var logger *logging.Logger
+		if name == "root" {
+			logger = logging.Root
+		} else {
+			logger = logging.Get(name)
+		}
+		logger.Threshold = level
+		// Handle extra options
+		for _, outputKey := range parts[1:] {
+			if outputKey == "nopropagate" {
+				logger.NoPropagate = true
+			} else {
+				// Assign an outputter
+				if outputter := outputters[outputKey]; outputter != nil {
+					logger.AddOutput(outputter)
+				} else {
+					return fmt.Errorf("Unknown logging output: %v", outputKey)
+				}
+			}
+		}
+	}
+
+	logging.Root.Configure()
+	logging.CustomSetup()
+	return nil
+}
--- src/codetag/main.go
+++ src/codetag/main.go
@@ -2,23 +2,31 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"flag"
 	"os"
+	"os/user"
+	"path/filepath"
 	"github.com/hoisie/mustache"
 	"github.com/vaughan0/go-logging"
+	"github.com/kylelemons/go-gypsy/yaml"
+	"codetag/log_setup"
 )
-	// "path/filepath"
 
 type path_t string
 func (path path_t) Render() string {
 	return fmt.Sprint(path)
 }
 
-var config_paths = []path_t{"~/.codetag.yaml", "/etc/codetag.yaml"}
-var config string
+var config_search = []path_t{"", "~/.codetag.yaml", "/etc/codetag.yaml"}
+var config_path string
+
+
+func configure_loggers(config interface{})
 
 
 func main() {
+	config_search[0] = path_t(os.Args[0] + ".yaml")
 
 	flag.Usage = func() {
 		fmt.Println(mustache.Render( `usage: {{{cmd}}} [ <options> ]
@@ -33,22 +41,92 @@ Examples:
   % {{{cmd}}}
   % {{{cmd}}} --config config.yaml
 
-Options:`, map[string]string{"cmd": os.Args[0]}, map[string][]path_t{"paths": config_paths} ))
+Options:`, map[string]string{"cmd": os.Args[0]}, map[string][]path_t{"paths": config_search} ))
 		flag.PrintDefaults()
 	}
 
-	flag.StringVar(&config, "config", "", "Configuration file to use.")
+	flag.StringVar(&config_path, "config", "", "Configuration file to use.")
 	flag.Parse()
 	if flag.NArg() > 0 {
-		fmt.Printf("Error: no command-line arguments are allowed (provided: %v)\n", flag.Args())
+		fmt.Fprintf(os.Stderr, "Error: no command-line arguments are allowed (provided: %v)\n", flag.Args())
 		os.Exit(1)
 	}
 
-	logging.DefaultSetup()
+	if len(config_path) == 0 {
+		for _, path := range config_search {
+			config_path = filepath.Clean(string(path))
+			if strings.HasPrefix(config_path, "~/") {
+				parts := strings.SplitN(config_path, "/", 2)
+				parts[0] = os.Getenv("HOME")
+				if len(parts[0]) == 0 {
+					user, err := user.Current()
+					if err != nil {
+						parts = nil
+					} else {
+						parts[0] = user.HomeDir
+					}
+				}
+				if parts != nil {
+					config_path = strings.Join(parts, "/")
+				} else {
+					config_path = ""
+				}
+			}
+			_, err := os.Stat(config_path)
+			if err == nil {
+				break
+			}
+			config_path = ""
+		}
+		if len(config_path) == 0 {
+			fmt.Fprintf(os.Stderr, "Failed to find any suitable configuration file")
+			os.Exit(1)
+		}
+	}
+
+	// fmt.Fprintf(os.Stderr, "Using config path: %#v", config_path)
+
+	config, err := yaml.ReadFile(config_path)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to process configuration file (%q): %#v", config_path, err)
+		os.Exit(1)
+	}
+
+
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to process configuration file (%q): %v", config_path, err)
+			os.Exit(1)
+		}
+	}()
+
 	log := logging.Get("codegen.main")
 
-	log.Infof("config: %#v", config)
+	conf_logging, err := yaml.Child(config.Root, ".logging")
+	if err != nil {
+		logging.DefaultSetup()
+		log.Warnf("Failed to setup logging: %#v", err)
+	} else {
+		func() {
+			conf_logging_map, ok := conf_logging.(yaml.Map)
+			if !ok {
+				logging.DefaultSetup()
+				log.Error("'logging' config section is not a map, ignoring")
+				return
+			}
+			err = log_setup.SetupYAML(&conf_logging_map)
+			if err != nil {
+				logging.DefaultSetup()
+				log.Errorf("Failed to configure logging: %v", err)
+			}
+		}()
+
+	}
+	log.Debug("Done!")
 }
+
+	// for key, section := range conf_logging
+	// 	fmt.Println(key, section)
 
 	// func walk_iter(path string, info os.FileInfo, err error) error
 	// filepath.Walk(root string, walkFn WalkFunc) error
