49511d5c1da761d8c5c325d6677b3a9034694afa
Mike Kazantsev <mk.fraggod@gmail.com>
2013-02-04 00:12:34 +0600

    Added path-based lang:* tagger

--- src/codetag/taggers/taggers.go
+++ src/codetag/taggers/taggers.go
@@ -4,9 +4,9 @@ import (
 	"path/filepath"
 	"os"
 	"fmt"
+	re "regexp"
 	"github.com/kylelemons/go-gypsy/yaml"
 )
-
 
 // Taggers are configurable routines that return a string tag(s) for a file,
 //  given it's location. What they do to that path (or files) is plugin-specific.
@@ -41,12 +41,12 @@ func Get(name string, config *yaml.Node) (Tagger, error) {
 
 
 // Assumes that there can be only one scm tag, so flushes previous tags if scm-path is detected.
-var scm_paths = map[string]string{"git": ".git", "hg": ".hg", "svn": ".svn", "bzr": ".bzr"}
+var scm_paths = map[string]string{".git": "git", ".hg": "hg", ".bzr": "bzr", ".svn": "svn"}
 func tagger_scm_detect_paths(name string, config *yaml.Node, path string, info os.FileInfo, ctx *map[string]interface{}) (tags []string) {
 	if !info.IsDir() {
 		return nil
 	}
-	for tag, dir := range scm_paths {
+	for dir, tag := range scm_paths {
 		info, err := os.Stat(filepath.Join(path, dir))
 		if err == nil && info.IsDir() {
 			if *ctx != nil {
@@ -59,7 +59,54 @@ func tagger_scm_detect_paths(name string, config *yaml.Node, path string, info o
 }
 
 
+type tag_pattern struct {
+	pattern *re.Regexp
+	tag string
+}
+
+var (
+	// Only more-or-less plaintext (greppable) files for now
+	lang_ext_map = map[string]string{//+as-is
+		"py|tac": "py", "go": "go", "c(pp|xx)?|h": "c", "js|coffee": "js",
+		"co?nf|cfg|ini": "conf", "unit|service|taget|mount|desktop|rules": "conf",
+		"x?htm(l[45]?)?|css": "html", "xml|xsl": "xml",
+		"patch|diff": "diff", "(ba|z|k|c|fi)?sh|env": "sh",
+		"p(l|m)": "perl", "php[45]?": "php", "[ce]l|lisp": "lisp", "hs": "haskell",
+		"md|markdown": "md", "rst": "rst",
+		"ya?ml": "yaml", "json(\\.txt)?": "json", "do": "redo", "mk|a[cm]": "make" }//-as-is
+	lang_path_map = map[string]string{//+as-is
+		"/config$": "conf", "/Makefile$": "make", "/zsh/_[^/]+$": "sh", "patch": "diff" }//-as-is
+	lang_regexps = []tag_pattern{}
+)
+
+func tagger_lang_detect_paths(name string, config *yaml.Node, path string, info os.FileInfo, ctx *map[string]interface{}) (tags []string) {
+	if info.IsDir() {
+		return nil
+	}
+	for _, filter := range lang_regexps {
+		if filter.pattern.Match([]byte(path)) {
+			tags = append(tags, filter.tag)
+		}
+	}
+	return
+}
+
+
 // map of available Tagger functions
 var taggers = map[string]tagger_func {
 	"scm_detect_paths": tagger_scm_detect_paths,
+	"lang_detect_paths": tagger_lang_detect_paths,
+}
+
+
+func init() {
+	// Compile patterns for tagger_lang_detect_paths
+	for re_base, tag := range lang_ext_map {
+		re_base = "\\.(" + re_base +
+			")(\\.(default|in|(src-)?bak|backup|example|sample|dist|\\w+-new))?$"
+		lang_regexps = append(lang_regexps, tag_pattern{re.MustCompile(re_base), tag})
+	}
+	for re_base, tag := range lang_path_map {
+		lang_regexps = append(lang_regexps, tag_pattern{re.MustCompile(re_base), tag})
+	}
 }
